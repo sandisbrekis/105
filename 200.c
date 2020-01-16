37d6e4edec6d8676c2b1b336491b7b2f160d2d44
sandisbrekis <58108670+sandisbrekis@users.noreply.github.com>
2020-01-16 09:35:14 +0200

    Update 200.c

--- 200.c
+++ 200.c
@@ -1,7 +1,21 @@
+/* 21.c
+Programmas autors: Sandis Brēķis 121REB474
+Programma veidota 2019. gada 23. novembrī
+Versija: 1.0
+Programma parāda iespējamo datu tipa pārplūdi */
+
 #include <stdio.h>
-int main () {
- int a=5;
-for( a ; a<10 ; a++){
-printf ("a = %d /n" , a);
-}
+#include <limits.h>
+int main()
+
+{
+int a = 50000;
+int b = 1000000;
+int c = a * b;
+long int c = (long int)a * (long int)b; //Sagaidamais rezultats
+
+ printf("int datu tipa izmers ir: %ld baiti \n" , sizeof(int) );
+ printf("Apreekjinam a un b reizinaajumu: \n" );
+ printf("a = %d, b = %d \n", a, b);
+ printf("c = a * b = %d * %d = %d \n" , a,b,c ); //Rezultats
 }
