435ca8608ab1220d4bbfe792d0411937465370c6
sandisbrekis <58108670+sandisbrekis@users.noreply.github.com>
2020-01-16 09:35:38 +0200

    Update 200.c

--- 200.c
+++ 200.c
@@ -1,21 +1,28 @@
-/* 21.c
+/* 200.c
 Programmas autors: Sandis Brēķis 121REB474
 Programma veidota 2019. gada 23. novembrī
 Versija: 1.0
-Programma parāda iespējamo datu tipa pārplūdi */
+Programma veic ciklisku x dalīšanu ar y */
 
 #include <stdio.h>
 #include <limits.h>
+
 int main()
-
 {
-int a = 50000;
-int b = 1000000;
-int c = a * b;
-long int c = (long int)a * (long int)b; //Sagaidamais rezultats
+ int x;
+ printf("Ievadi skaitli:\t"); // Skaitla x ievade
+ scanf("%d", &x);
+ printf("Ievadītais skaitlis ir: %d\n", x);
 
- printf("int datu tipa izmers ir: %ld baiti \n" , sizeof(int) );
- printf("Apreekjinam a un b reizinaajumu: \n" );
- printf("a = %d, b = %d \n", a, b);
- printf("c = a * b = %d * %d = %d \n" , a,b,c ); //Rezultats
+ for (x; x<=10; x++)
+{
+ for (int y=1; y<=10; y++)
+{
+ double z = ((float)x / (float)y); // Dalijums
+ printf("X vērtība ir: %d, Y vērtība ir: %d\n",x,y);
+ printf("Dalījums ir: %.2f\n", z);	
+}
+}
+
+
 }
