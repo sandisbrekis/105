b5714aba676d2d4ec8c736d5339cda984e7fcf31
sandisbrekis <58108670+sandisbrekis@users.noreply.github.com>
2020-01-16 09:36:40 +0200

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,14 +1,20 @@
+/* 22.c
+Programmas autors: Sandis Brēķis 121REB474
+Programma veidota 2019. gada 23. novembrī
+Versija: 1.0
+Programma cikliskam robežas e aprēķinam */
+
 #include <stdio.h>
-#include <math.h>
-
+#include <limits.h>
 int main()
-{
- int k;
- double lim;
 
- for (k=1; k<=50; k++ )
- {
-  lim = pow((1+1./k),k);
-  printf("k=%d lim=%f\n", k, lim);
- }
+{
+ int k; // Mainigais Cikla
+ double lim; // Rebezas mainigais
+
+ for (k=1; k<=50; k++)
+{
+ lim = pow((1+1./k),k); // Aprekins
+ printf ("k=%d lim =%f \n", k, lim);
+}
 }
